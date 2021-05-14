6c477d537667b6a49451c787ab0396305ed14350
윤현준 <kkkths@naver.com>
2021-05-14 14:26:27 +0900

    create

---
+++ src/test/java/com/yhj/board/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+//package com.yhj.board.web.dto;
+//
+//import org.junit.Test;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class HelloResponseDtoTest {
+//
+//    @Test
+//    public void lombok_test() {
+//        String name = "test";
+//        int amount = 1000;
+//
+//        HelloResponseDto dto = new HelloResponseDto(name, amount);
+//
+//        assertThat(dto.getName()).isEqualTo(name);
+//        assertThat(dto.getAmount()).isEqualTo(amount);
+//    }
+//
+//
+//}
