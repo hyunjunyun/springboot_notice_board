18ab91b8ae0c10a9c5dff596f750a09959c2199a
윤현준 <kkkths@naver.com>
2021-05-28 02:28:48 +0900

    테스트 성공

--- src/test/java/com/yhj/board/web/HelloControllerTest.java
+++ src/test/java/com/yhj/board/web/HelloControllerTest.java
@@ -1,7 +1,9 @@
 package com.yhj.board.web;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,8 +16,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
+
+    @Autowired
     private MockMvc mvc;
 
+    @Test
     public void return_hello() throws Exception {
         String hello = "hello";
 
