29612a9a57029e006d4c74e966a7b9165f85dd3c
윤현준 <kkkths@naver.com>
2021-05-28 02:28:07 +0900

    Controller annotation 추가

--- src/main/java/com/yhj/board/web/HelloController.java
+++ src/main/java/com/yhj/board/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.yhj.board.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
