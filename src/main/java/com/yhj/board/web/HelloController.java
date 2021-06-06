59732af4c318a1259ae16eda91d5a3b7f7d5ac06
윤현준 <kkkths@naver.com>
2021-06-06 23:51:29 +0900

    Update HelloController.java

--- src/main/java/com/yhj/board/web/HelloController.java
+++ src/main/java/com/yhj/board/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.yhj.board.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "in2dex";
     }
+
+//    @GetMapping("/hello/dto")
+//    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+//        return new HelloResponseDto(name, amount);
+//    }
 }
